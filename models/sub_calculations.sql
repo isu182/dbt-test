bf3c49b5327a8fb6bfb5d9b9776f20246e2e12ce
Mályár István <malyaristvan@gmail.com>
2021-08-06 10:54:29 +0200

    Update sub_calculations.sql

--- models/sub_calculations.sql
+++ models/sub_calculations.sql
@@ -1,1 +1,23 @@
-select id from {{ ref('sub_calculations') }} limit 10
+with dbt_test as (
+
+    select
+        client_user_id as client_id
+        calculation_type as type
+        created_at as date
+
+    from {{ source('money_check', 'sub_calculations') }}
+
+),
+
+
+final as (
+
+    select
+        dbt_test.client_id,
+        dbt_test.type,
+        dbt_test.date
+
+    from dbt_test
+)
+
+select * from final
